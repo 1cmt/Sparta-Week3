152931419df9a97d6656e47291b0075e6bb98e2d
TunnelSight <167070589+TunnelSight@users.noreply.github.com>
2024-05-01 12:05:10 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,12 +5,14 @@ namespace TextGame // Note: actual namespace depends on the project name.
 {
     internal class GameManager
     {
+        public List<Quest> quests;
         public GameManager()
         {
             InitializeGame();
          }
         public void InitializeGame()
         {
+            quests = Quest.GetInitialQuests();
             //저장한 데이터를 불러오는 과정
         }
         public void StartGame()
@@ -23,10 +25,12 @@ namespace TextGame // Note: actual namespace depends on the project name.
         private void MainMenu()
         {
             //프로그램 진행과정 
+            
         }
 
         public static void Main()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             GameManager gamemanager = new GameManager();
             gamemanager.StartGame();
         }
