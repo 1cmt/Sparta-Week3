de050fcd8b386c4b5eb9170a590c290174388872
1CM <lcm2009@naver.com>
2024-05-01 14:42:57 +0900

    [Fix] Inventory.cs
    장치 장착 관련 수정

--- Player.cs
+++ Player.cs
@@ -20,8 +20,7 @@ namespace Sparta_week3
         public int Level;
         public float Atk;
         public int Def;
-        public AtkItem? atkItem;
-        public DefItem? defItem;
+        public int MaxHp;
         public int Ctl;
         public int Mana;
         public int Hp;
@@ -38,9 +37,8 @@ namespace Sparta_week3
             _job = job;
             Level = level;
             Gold = gold;
+            MaxHp = hp;
             Hp = hp;
-            atkItem = null;
-            defItem = null;
             if(job == "Archer") Atk = (int)(atk * 1.2f); 
             else Atk = atk;
             Def = job == "Warrior" ? (int)(def * 1.2f) : def;
