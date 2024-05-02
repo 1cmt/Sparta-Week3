0d0c9bc42b6075e43f77a473dd1945e014545d20
leemoojin <hzm_deli@naver.com>
2024-05-02 15:04:19 +0900

    Merge branch 'Develop' into Develop-LMJ(Dungeon_Monster)
    퀘스트 클래스 관련 작업 머지

--- Player.cs
+++ Player.cs
@@ -18,17 +18,19 @@ namespace TextGame
         private string _job;
         public string Job { get { return _job; } set => _job = value; }
         public int Level;
-        public float Atk;
+        public int Atk;
         public int Def;
         public float Ctl = 0.15f;
-        public int Mana=50;
+        public int Mp;
         public int MaxHp;
         public int Hp;
+        public int MaxMp;
         public int Gold;
         public int Cexp=0;
+        public List<int> KillCount = new List<int> { 0, 0, 0, 0 };
         public int Texp { get; private set; }
 
-        public Player(string name, string job, int level = 1, int atk = 10, int def = 10, int hp = 100, int gold = 1000, int exp = 10)
+        public Player(string name, string job, int level = 1, int atk = 10, int def = 10, int hp = 100, int gold = 1000, int exp = 10,int mp =50,int maxmp=50)
         {
             _name = name;
             _job = job;
@@ -36,52 +38,43 @@ namespace TextGame
             Gold = gold;
             (MaxHp,Hp) = (hp,hp);
             Texp = exp;
+            Mp = mp; MaxMp = maxmp;
             if (job == "archer") Atk = (int)(atk * 1.2f);
             else Atk = atk;
             Def = job == "warrior" ? (int)(def * 1.2f)  : def;
             (Hp,MaxHp)  = job == "warrior" ? ((int)(hp * 1.2f), (int)(hp * 1.2f)) : (hp,hp);
             Ctl = job == "assassin"? (int)(Ctl * 1.2f)  : Ctl;
-            Mana= job == "wizard"  ? (int)(Mana * 1.2f) : Mana;
+            (Mp,MaxMp)= job == "wizard"  ? ((int)(Mp * 1.2f), (int)(Mp * 1.2f)) : (Mp,Mp);
         }
 
         public void Levelup(ref int level, ref int cexp)
         {
-            if (cexp == Texp)
+            if (cexp >= Texp)
             {
+                cexp -= Texp;
                 Texp += 20 + level * 5;
-                Atk += 0.5f;
+                Atk += 1;
                 Def ++;
                 level++;
-                cexp = 0;
+                ;
             }
         }
-        public void Changejob(ref int gold, string job)
+        public static string CheckJob()
         {
-            if (gold >= 32747)
-            { 
-                gold -= 32747;
-                Console.Write("변경하실 직업을 입력해주세요: ");
-                string TempJob = Console.ReadLine();
-                if(job == TempJob) { Console.Write("똑같은 직업입니다."); }
-                else { 
-                        job = TempJob.ToUpper();
-                         
-                     }
-                Console.Read();
-            }
-            else
+            string @as = "ASSASSIN"; string wa = "WARRIOR"; string wi = "WIZARD"; string ar = "ARCHER";
+            string? job = Console.ReadLine().ToUpper();
+            while (job != @as & job != wa & job != wi & job != ar)
             {
-                Console.WriteLine($"골드가 {32747-gold}만큼 부족합니다");
-                Console.ReadKey();
+                Console.WriteLine("올바른 직업을 선택해주세요");
+                job = Console.ReadLine().ToUpper();
             }
+            return job;
         }
         public static string InputName()
         {
             Console.Write("B1A4던전에 오신 것을 환영합니다 이름을 적어주세요 : ");
             string? Name = Console.ReadLine();
-#pragma warning disable CS8603 // 가능한 null 참조 반환입니다.
             return Name;
-#pragma warning restore CS8603 // 가능한 null 참조 반환입니다.
         }
 
         internal static string InputJob()
@@ -95,16 +88,29 @@ namespace TextGame
             Console.Write("치명적인 일격을 가합니다   "+"생존능력이 높습니다        "+"                           "+"높은 데미지를 입힙니다     "+"\n");
             ConsoleUtility.PrintLine('=');
 
-            string? job = Console.ReadLine();
-            job = job.ToUpper();
-            while(job != @as& job!= wa& job != wi & job != ar) 
-            {
-                Console.WriteLine("올바른 직업을 선택해주세요");
-                job = Console.ReadLine();
-                job = job.ToUpper();
-            }
-            return job;
+            string Tempjob = CheckJob();
+            return Tempjob ;
 
+        }
+        public void Changejob(ref int gold, ref string job)
+        {
+            if (gold >= 32747)
+            {
+                Console.Write("변경하실 직업을 입력해주세요: ");
+                string? TempJob = CheckJob();
+                if (job == TempJob) { Console.WriteLine("똑같은 직업입니다."); }
+                else
+                {
+                    job = TempJob;
+                    gold -= 32747;
+                    Console.WriteLine(TempJob + "직업으로 변경완료");
+                }
+            }
+            else { Console.WriteLine($"골드가 {32747 - gold}만큼 부족합니다"); }
+
+            ConsoleUtility.PrintLine('=');
+            Console.WriteLine("아무 키나 눌러서 진행");
+            Console.ReadKey();
         }
         internal void StatusMenu(Player player,Inventory inventory)
         {
@@ -116,32 +122,25 @@ namespace TextGame
             ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "공격력 : ",player.Atk.ToString(),"\n");
             ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "방어력 : ", player.Def.ToString(),"\n");
             ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "체력 : ", player.Hp.ToString(),"\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "Gold : ", player.Gold.ToString(),"\n");
+            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "Gold : ", player.Gold.ToString(),"\n\n");
 
             ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "", "0", ". 메뉴로 나가기\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "", "1", ". 직업변경 : 32767 G");
-
+            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "", "1", ". 직업변경 : 32767 G\n");
+            ConsoleUtility.PrintLine('=');
             int choice = ConsoleUtility.PromptMenuChoice(0, 1);
             if (choice == 0) return;
             else
             {
-                Changejob(ref player.Gold, player.Job);
+                Changejob(ref player.Gold, ref player._job);
                 StatusMenu(player,inventory);
             }
-           
-
-            
-           
-
-
-
         }
 
 
 
         public void Attack(Monster monster)
         {
-            monster.Hp -= (int)Atk;
+            monster.Hp -= Atk;
             if (monster.Hp < 0) monster.Hp = 0;
         }
     }
