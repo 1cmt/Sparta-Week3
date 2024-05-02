344b3b52c5e30e514117d84cc1ef3a2641eebf1d
leemoojin <hzm_deli@naver.com>
2024-05-02 21:24:58 +0900

    [Fix] Player.cs

--- Player.cs
+++ Player.cs
@@ -21,35 +21,32 @@ namespace TextGame
         public int Atk;
         public int Def;
         public float Ctl = 0.15f;
-        public int Mana=50; 
+        public int Mp;
         public int MaxHp;
         public int Hp;
         public int MaxMp;
         public int Gold;
-        public int Cexp=0;
+        public int Cexp = 0;
         public List<int> KillCount = new List<int> { 0, 0, 0, 0 };
-        public Item?[] EquipItems { get; set; } //배열의 index가 장착 부위를 의미 (ItemType을 int로 형변환 하여 접근)
-
         public int Texp { get; private set; }
         public List<Skill> skillbook = new List<Skill>();
 
-        public Player(string name, string job, int level = 1, int atk = 10, int def = 10, int hp = 100, int gold = 10000, int exp = 10)
+        public Player(string name, string job, int level = 1, int atk = 10, int def = 10, int hp = 100, int gold = 1000, int exp = 10, int mp = 50, int maxmp = 50)
         {
             _name = name;
             _job = job;
             skillbook = Skill.Gainskill(job);
             Level = level;
             Gold = gold;
-            (MaxHp,Hp) = (hp,hp);
+            (MaxHp, Hp) = (hp, hp);
             Texp = exp;
             Mp = mp; MaxMp = maxmp;
             if (job == "archer") Atk = (int)(atk * 1.2f);
             else Atk = atk;
-            Def = job == "warrior" ? (int)(def * 1.2f)  : def;
-            (Hp,MaxHp)  = job == "warrior" ? ((int)(hp * 1.2f), (int)(hp * 1.2f)) : (hp,hp);
-            Ctl = job == "assassin"? (int)(Ctl * 1.2f)  : Ctl;
-            Mana= job == "wizard"  ? (int)(Mana * 1.2f) : Mana;
-            EquipItems = new Item?[10];
+            Def = job == "warrior" ? (int)(def * 1.2f) : def;
+            (Hp, MaxHp) = job == "warrior" ? ((int)(hp * 1.2f), (int)(hp * 1.2f)) : (hp, hp);
+            Ctl = job == "assassin" ? (int)(Ctl * 1.2f) : Ctl;
+            (Mp, MaxMp) = job == "wizard" ? ((int)(Mp * 1.2f), (int)(Mp * 1.2f)) : (Mp, Mp);
         }
 
         public void Levelup(ref int level, ref int cexp)
@@ -59,7 +56,7 @@ namespace TextGame
                 cexp -= Texp;
                 Texp += 20 + level * 5;
                 Atk += 1;
-                Def ++;
+                Def++;
                 level++;
                 ;
             }
@@ -88,20 +85,20 @@ namespace TextGame
 
             ConsoleUtility.PrintLine('=');
             string @as = "ASSASSIN"; string wa = "WARRIOR"; string wi = "WIZARD"; string ar = "ARCHER";
-            Console.Write("       "+@as.PadRight(27)+ wa.PadRight(27)+wi.PadRight(27)+ ar.PadRight(22)+"\n");
-            Console.Write("상대의 급소를 노려         "+"강인한 육체의 소유자로     "+"강력한 스킬을 사용합니다   "+"강력한 팔힘으로            "+"\n");
-            Console.Write("치명적인 일격을 가합니다   "+"생존능력이 높습니다        "+"                           "+"높은 데미지를 입힙니다     "+"\n");
+            Console.Write("       " + @as.PadRight(27) + wa.PadRight(27) + wi.PadRight(27) + ar.PadRight(22) + "\n");
+            Console.Write("상대의 급소를 노려         " + "강인한 육체의 소유자로     " + "강력한 스킬을 사용합니다   " + "강력한 팔힘으로            " + "\n");
+            Console.Write("치명적인 일격을 가합니다   " + "생존능력이 높습니다        " + "                           " + "높은 데미지를 입힙니다     " + "\n");
             ConsoleUtility.PrintLine('=');
 
             string Tempjob = CheckJob();
-            return Tempjob ;
+            return Tempjob;
 
         }
         public void Changejob(ref int gold, ref string job)
         {
             gold = 34000;
             if (gold >= 32747)
-            {      
+            {
                 Console.Write("변경하실 직업을 입력해주세요: ");
                 string? TempJob = InputJob();
                 if (job == TempJob) { Console.WriteLine("똑같은 직업입니다."); }
@@ -118,18 +115,18 @@ namespace TextGame
             Console.WriteLine("아무 키나 눌러서 진행");
             Console.ReadKey();
         }
-        internal void StatusMenu(Player player,Inventory inventory)
+        internal void StatusMenu(Player player, Inventory inventory)
         {
             Console.Clear();
             ConsoleUtility.PrintTitle(((Func<string, string>)(status => status.PadLeft(50)))("상태창"));
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "레벨 : ", player.Level.ToString(),"\n");
+            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "레벨 : ", player.Level.ToString(), "\n");
             ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "이름 : ", player.Name);
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "(", player.Job,")\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "공격력 : ",player.Atk.ToString(),"\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "방어력 : ", player.Def.ToString(),"\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "체력 : ", player.Hp.ToString(),"\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "Gold : ", player.Gold.ToString(),"\n\n");
-            for(int i =0; i<player.skillbook.Count; i++) 
+            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "(", player.Job, ")\n");
+            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "공격력 : ", player.Atk.ToString(), "\n");
+            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "방어력 : ", player.Def.ToString(), "\n");
+            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "체력 : ", player.Hp.ToString(), "\n");
+            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "Gold : ", player.Gold.ToString(), "\n\n");
+            for (int i = 0; i < player.skillbook.Count; i++)
             {
                 player.skillbook[i].PrintSkillDescription(player);
             }
@@ -141,40 +138,16 @@ namespace TextGame
             else
             {
                 Changejob(ref player.Gold, ref player._job);
-                StatusMenu(player,inventory);
+                StatusMenu(player, inventory);
             }
         }
 
-        internal void StatusMenu(Inventory inventory)
-        {
-            Console.Clear();
 
-            ConsoleUtility.PrintTitle(((Func<string, string>)(status => status.PadLeft(50)))("상태창".PadLeft(50)));
-            //ConsoleUtility.PrintTitle("상태창".PadLeft(50));
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "레벨 : ", Level.ToString(),"\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "이름 : ", Name);
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "(", Job,")\n");
-
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Green, "공격력 : ", (Atk + inventory.BonusAtk).ToString().PadRight(6), inventory.BonusAtk > 0 ? $" (+{inventory.BonusAtk})\n" : "\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Green, "방어력 : ", (Def + inventory.BonusDef).ToString().PadRight(6), inventory.BonusDef > 0 ? $" (+{inventory.BonusDef})\n" : "\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Green, "체  력 : ", (Hp + inventory.BonusHp).ToString().PadRight(6), inventory.BonusHp > 0 ? $" (+{inventory.BonusHp})\n" : "\n");
-            ConsoleUtility.PrintTextHighlightsColor(ConsoleColor.Yellow, "Gold : ", Gold.ToString(),"\n");
 
         public void Attack(Monster monster)
         {
             monster.Hp -= Atk;
             if (monster.Hp < 0) monster.Hp = 0;
-            Console.WriteLine("");
-            Console.WriteLine("0. 나가기");
-            Console.WriteLine("");            
-
-            switch (ConsoleUtility.PromptMenuChoice(0, 0))
-            {
-                case 0:
-                    return;
-            }
-
-            Thread.Sleep(10000);
         }
     }
 }
